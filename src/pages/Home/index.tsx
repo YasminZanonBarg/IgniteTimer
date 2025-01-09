f591c800e8b861704b68d83658a5273853a06529
yasmin.barg <yasmin.barg@havan.com.br>
2025-01-09 10:54:36 -0300

    Adding static home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,52 @@
+import { Play } from "phosphor-react";
+import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
+
+export function Home(){
+    return(
+        <HomeContainer>
+            <form action="">
+                <FormContainer>
+                    <label htmlFor="task">Vou trabalhar em</label>
+                    <TaskInput 
+                        type="text" 
+                        id="task" 
+                        list="task-suggestion"
+                        placeholder="Dê um nome para o seu projeto" 
+                    />
+
+                    <datalist id="task-suggestion">
+                        <option value="Projeto 1" />
+                        <option value="Projeto 2" />
+                        <option value="Projeto 3" />
+                        <option value="Projeto 4" />
+                    </datalist>
+
+                    <label htmlFor="minutesAmount">durante</label>
+                    <MinutesAmountInput 
+                        type="number" 
+                        id="minutesAmount" 
+                        placeholder="00"
+                        step={5} // propriedade que permite pular o contator
+                        min={5} // valor minímo do contator
+                        max={60} // valor máximo do contator
+                    />
+
+                    <span>minutes</span>
+                </FormContainer>
+
+                <CountdownContainer>
+                    <span>0</span>
+                    <span>0</span>
+                    <Separator>:</Separator>
+                    <span>0</span>
+                    <span>0</span>
+                </CountdownContainer>
+
+                <StartCountdownButton disabled type="submit">
+                    <Play size={24} />
+                    Começar
+                </StartCountdownButton>
+            </form>
+        </HomeContainer>
+    );
+}
